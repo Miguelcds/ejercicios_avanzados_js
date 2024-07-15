22d9df6b3497100b227af897ac2ef571fe1cb564
Joao Miguel Costa Da Silva <miguel-cs@hotmail.com>
2024-07-15 22:59:27 +0200

    4

---
+++ Ejercio4.js
@@ -0,0 +1,21 @@
+function findArrayIndex(array, text) {
+ 
+    let position = array.indexOf(text)
+    
+    console.log(position +1, "If count like a Human" );
+    console.log(position, "Like a Robot");
+    }
+    
+    const mainCharacters = [
+    "Luke",
+    "Leia",
+    "Han Solo",
+    "Chewbacca",
+    "Rey",
+    "Anakin",
+    "Obi-Wan",
+    ];
+    
+    
+    
+    findArrayIndex(mainCharacters, "Leia")
